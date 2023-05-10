e6dccd1835b47bd3229592731d153babb7a88e6e
Dennis Galvén <dennis@dennisaurus.dev>
2023-05-10 10:59:22 +0700

    update release flow

--- lib/fluvvm.dart
+++ lib/fluvvm.dart
@@ -1,5 +1,5 @@
 export 'package:fluvvm/src/network_request.dart'
-    show NetworkError, NetworkMethod, NetworkRequest;
+    show NetworkError, NetworkMethod, NetworkRequest, NetworkRequestError;
 export 'package:fluvvm/src/notified_widget.dart' show NofifiedWidget;
 export 'package:fluvvm/src/viewmodel.dart'
     show FluvvmIntent, FluvvmState, Viewmodel;
